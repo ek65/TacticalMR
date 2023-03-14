1602b7fa396f34818a33f0b0dff33ea0a5bc03c9
Daniel He <tcdanielh@berkeley.edu>
2023-03-14 06:15:01 -0700

    completed demos

---
+++ passthrough test5/Assets/Demo2.cs
@@ -0,0 +1,146 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Demo2 : MonoBehaviour
+{
+    /* This is not great way to do it, but having a bool check is probably best right now.
+     Will be easier to port to scenic pipeline doing it this way of having a separate 
+    script call functions in the player move script. */
+    public MoveToSoccerBallAndTurn player1;
+    public MoveToSoccerBallAndTurn player2;
+    public player1Behavior player1behavior;
+    public player2Behavior player2behavior;
+
+    public enum player1Behavior
+    {
+        player1Idle1,
+        player1MoveToBall,
+        player1Idle2,
+        player1KickBallToPos1,
+        player1MoveToPos,
+        player1WaitForBall,
+        player1KickBallToPos2
+    };
+    
+    public enum player2Behavior
+    {
+        player2WaitForBall,
+        player2Idle1,
+        player2KickBallToPos
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Player1Behavior();
+        Player2Behavior();
+    }
+
+    void Player1Behavior()
+    {
+        if (player1.action == MoveToSoccerBallAndTurn.Action.NOT_TAKING_ACTION 
+            || player1.action == MoveToSoccerBallAndTurn.Action.TAKING_RECURRING_ACTION)
+        {
+            switch (player1behavior)
+            {
+                case player1Behavior.player1Idle1:
+                    StartCoroutine(player1.IdleForSec(1));
+                    player1behavior = player1Behavior.player1MoveToBall;
+                    break;
+                case player1Behavior.player1MoveToBall:
+                    player1.MoveToBallThenLook(player2.transform.position); //param takes in Vector3 where to look after
+                    if (player1.distToBall == 0)
+                    {
+                        player1behavior = player1Behavior.player1Idle2;
+                    }
+                    break;
+                case player1Behavior.player1Idle2:
+                    StartCoroutine(player1.IdleForSec(3));
+                    player1behavior = player1Behavior.player1KickBallToPos1;
+                    break;
+                case player1Behavior.player1KickBallToPos1:
+                    player1.KickBall(player2.transform.position);
+                    player1behavior = player1Behavior.player1MoveToPos;
+                    break;
+                case player1Behavior.player1MoveToPos:
+                    player1.speed = 5; //making him faster here for debugging ease
+                    Vector3 targetPos = new Vector3(0, 0, 10);
+                    player1.MoveToPosThenLook(targetPos, player1.goal.position); //2nd param takes in Vector3 where to look after
+                    if (player1.distToPos == 0)
+                    {
+                        player1behavior = player1Behavior.player1WaitForBall;
+                    }
+                    break;
+                case player1Behavior.player1WaitForBall:
+                    player1.WaitForBall();
+                    if (player1.distToBall == 0)
+                    {
+                        player1behavior = player1Behavior.player1KickBallToPos2;
+                    }
+                    break;
+                case player1Behavior.player1KickBallToPos2:
+                    player1.KickBall(player1.goal.position);
+                    break;
+            }
+        }
+
+        
+    }
+
+    void Player2Behavior()
+    {
+        if (player2.action == MoveToSoccerBallAndTurn.Action.NOT_TAKING_ACTION 
+            || player2.action == MoveToSoccerBallAndTurn.Action.TAKING_RECURRING_ACTION)
+        {
+            switch (player2behavior)
+            {
+                case player2Behavior.player2WaitForBall:
+                    player2.WaitForBall();
+                    if (player2.distToBall == 0)
+                    {
+                        player2behavior = player2Behavior.player2Idle1;
+                    }
+                    break;
+                case player2Behavior.player2Idle1:
+                    StartCoroutine(player2.IdleForSec(3));
+                    player2behavior = player2Behavior.player2KickBallToPos;
+                    break;
+                case player2Behavior.player2KickBallToPos:
+                    player2.KickBall(player1.transform.position);
+                    break;
+            }
+        }
+    }
+    // public IEnumerator KickWait()
+    // {
+    //     //Debug.Log("in here");
+    //     yield return new WaitForSeconds(3);
+    //     player1MoveToPos = true;
+    // }
+
+    //player1 behavior
+    //idle 1 second
+    //MoveTo ball
+    //idle 3 seconds
+    //KickBallToPosition (player2)
+    //idle 1 seconds
+    //MoveTo (0, 0, 10)
+    //WaitForBall
+    //KickBallToPosition (goal)
+
+    //player2 behavior
+    //WaitForBall
+    //idle 1 second
+    //KickBallToPosition (0, 0, 10)
+    
+    //try enums
+    //try have a list of actions and index through them
+}
--- passthrough test5/Assets/Scripts/MoveToSoccerBallAndTurn.cs
+++ passthrough test5/Assets/Scripts/MoveToSoccerBallAndTurn.cs
@@ -3,71 +3,179 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine.Animations.Rigging;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MoveToSoccerBallAndTurn : MonoBehaviour
 {
     public float speed;
     public Transform ball;
-    private Vector3 ballOnTheGround;
+    public Vector3 ballOnTheGround;
     public Transform mainCamera;
     public Rig animationRigging;
     private Animator anim;
-    private Collider cl;
-    private Vector3 correctPosition;
+    public Collider cl;
+    //private Vector3 correctPosition;
     public Vector3 targetPosition;
     public float force;
-    public bool getBall;
-    public bool travelWithBall;
+    //public bool getBall;
+    //public bool travelWithBall;
     public bool turn;
-    public Vector3 travelEndPos;
+    //public Vector3 travelEndPos;
+    public float distToBall;
+    public float distToPos;
+    public Transform goal;
+    public Action action;
+
+    public enum Action
+    {
+        NOT_TAKING_ACTION,
+        TAKING_ACTION,
+        TAKING_RECURRING_ACTION
+    };
 
     private void Start()
     {
-        ballOnTheGround.x = ball.transform.position.x;
-        ballOnTheGround.y = 0;
-        ballOnTheGround.z = ball.transform.position.z;
-        transform.LookAt(ballOnTheGround);
-        correctPosition = calculateCorrectPosition(transform.position, ballOnTheGround);
+        // ballOnTheGround.x = ball.transform.position.x;
+        // ballOnTheGround.y = 0;
+        // ballOnTheGround.z = ball.transform.position.z;
+        // transform.LookAt(ballOnTheGround);
+        // correctPosition = calculateCorrectPosition(transform.position, ballOnTheGround);
         anim = GetComponent<Animator>();
         cl = GetComponent<Collider>();
+        cl.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        ballOnTheGround.x = ball.transform.position.x;
+        ballOnTheGround.y = 0;
+        ballOnTheGround.z = ball.transform.position.z;
+        distToBall = Vector3.Distance(transform.position, ballOnTheGround);
+        // anim.ResetTrigger("reachedBall");
+        // cl.enabled = false;
+        // float step = speed * Time.deltaTime;
+        // transform.position = Vector3.MoveTowards(transform.position, correctPosition, step);
+        // if (transform.position == correctPosition && getBall && travelWithBall)
+        // {
+        //     TravelWithBall();
+        // }
+        //
+        // if (transform.position == travelEndPos)
+        // {
+        //     travelWithBall = false;
+        // }
+        // if (transform.position == correctPosition && getBall && !travelWithBall)
+        // {
+        //     cl.enabled = true;
+        //     if (turn)
+        //     {
+        //         TurnAround();
+        //         turn = false;
+        //     }
+        //     anim.SetTrigger("reachedBall");
+        // }
+    }
 
-        anim.ResetTrigger("reachedBall");
-        cl.enabled = false;
-        float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, correctPosition, step);
-        if (transform.position == correctPosition && getBall && travelWithBall)
-        {
-            TravelWithBall();
-        }
-
-        if (transform.position == travelEndPos)
-        {
-            travelWithBall = false;
-        }
-        if (transform.position == correctPosition && getBall && !travelWithBall)
-        {
-            cl.enabled = true;
-            if (turn)
-            {
-                TurnAround();
-                turn = false;
-            }
-            anim.SetTrigger("reachedBall");
-        }
+    public IEnumerator IdleForSec(int sec, System.Action<bool, bool> callback)
+    {
+        //Debug.Log("in here");
+        yield return new WaitForSeconds(sec);
+        callback(false, true);
+    }
+    
+    public IEnumerator IdleForSec(float sec)
+    {
+        action = Action.TAKING_ACTION;
+        //Debug.Log("in here");
+        yield return new WaitForSeconds(sec);
+        action = Action.NOT_TAKING_ACTION;
     }
 
     Vector3 calculateCorrectPosition(Vector3 pos1, Vector3 pos2)
     {
         float xPosDiff = pos2.x - pos1.x;
         float zPosDiff = pos2.z - pos1.z;
-        float distance = Mathf.Pow(Mathf.Pow(xPosDiff, 2) + Mathf.Pow(zPosDiff, 2), (float)0.5) + (float)0.2;
+        distToBall = Mathf.Pow(Mathf.Pow(xPosDiff, 2) + Mathf.Pow(zPosDiff, 2), (float)0.5) + (float)0.2;
         Vector3 normalizedDirection = Vector3.Normalize(Vector3.MoveTowards(transform.position, ballOnTheGround, 1));
-        return normalizedDirection * (distance);
+        return normalizedDirection * (distToBall);
+    }
+
+    public void MoveToBallThenLook(Vector3 lookAtPos)
+    {
+        action = Action.TAKING_RECURRING_ACTION;
+        anim.Play("Running");
+        transform.LookAt(ballOnTheGround);
+        //correctPosition = calculateCorrectPosition(transform.position, ballOnTheGround);
+
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, ballOnTheGround, step);
+        if (distToBall == 0)
+        {
+            anim.Play("Idle");
+            transform.LookAt(lookAtPos);
+            action = Action.NOT_TAKING_ACTION;
+        }
+    }
+    
+    public void MoveToPosThenLook(Vector3 moveToPos, Vector3 lookAtPos)
+    {
+        action = Action.TAKING_RECURRING_ACTION;
+        anim.Play("Running");
+        transform.LookAt(moveToPos);
+        distToPos = Vector3.Distance(transform.position, moveToPos);
+        //correctPosition = calculateCorrectPosition(transform.position, ballOnTheGround);
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, moveToPos, step);
+        if (distToPos == 0)
+        {
+            anim.Play("Idle");
+            transform.LookAt(lookAtPos);
+            action = Action.NOT_TAKING_ACTION;
+        }
+    }
+
+    public void KickBall(Vector3 pos)
+    {
+        action = Action.TAKING_ACTION;
+        targetPosition = pos;
+        anim.Play("Kick Soccerball");
+    }
+
+    // called by animation event in "Kick Soccerball"
+    void Kick()
+    {
+
+        ball.GetComponent<Rigidbody>().AddForce((targetPosition - transform.position).normalized * force, ForceMode.Impulse);
+    }
+
+    // called by animation event in "Kick Soccerball"
+    private void AfterKick()
+    {
+        //Debug.Log("test");
+        anim.Play("Idle");
+        action = Action.NOT_TAKING_ACTION;
+    }
+
+    public void WaitForBall()
+    {
+        action = Action.TAKING_RECURRING_ACTION;
+        if (distToBall <= 0.1f)
+        {
+            StopBall();
+            action = Action.NOT_TAKING_ACTION;
+        }
+    }
+
+    public void StopBall()
+    {
+        ball.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        ball.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        Vector3 temp = transform.position;
+        temp.x = transform.position.x;
+        temp.z = transform.position.z;
+        ball.transform.position = temp;
+        
     }
   
     // Turn towards main player's camera.
@@ -76,26 +184,47 @@ public class MoveToSoccerBallAndTurn : MonoBehaviour
         animationRigging.weight = 1;
     }
 
-    void TravelWithBall()
+    public void TravelWithBall(Vector3 moveToPos, Vector3 lookAtPos)
     {
-        cl.enabled = true;
-        correctPosition = new Vector3(ball.transform.position.x, 0, ball.transform.position.z);
-        if (turn)
+        //cl.enabled = true;
+        //correctPosition = new Vector3(ball.transform.position.x, 0, ball.transform.position.z);
+        // if (turn)
+        // {
+        //     transform.LookAt(mainCamera);
+        //     //TurnAround();
+        //     turn = false;
+        // }
+        action = Action.TAKING_RECURRING_ACTION;
+        anim.Play("Running");
+        transform.LookAt(moveToPos);
+        distToPos = Vector3.Distance(transform.position, moveToPos);
+        
+        targetPosition = moveToPos;
+
+        if (distToBall <= 0.1f)
         {
-            transform.LookAt(mainCamera);
-            //TurnAround();
-            turn = false;
+            float tempForce = 1f;
+            ball.GetComponent<Rigidbody>().AddForce((targetPosition - transform.position).normalized * tempForce, ForceMode.Impulse);
         }
-        force = 3;
+
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, moveToPos, step);
+        if (distToPos == 0)
+        {
+            anim.Play("Idle");
+            StopBall();
+            transform.LookAt(lookAtPos);
+            action = Action.NOT_TAKING_ACTION;
+        }
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.name == "soccer_ball")
-        { 
-            //targetPosition = Vector3.forward;
-            targetPosition = (mainCamera.transform.position - transform.position).normalized;
-            ball.GetComponent<Rigidbody>().AddForce(targetPosition * force, ForceMode.Impulse);
-        }
-    }
+    // private void OnCollisionEnter(Collision collision)
+    // {
+    //     if (collision.gameObject.name == "soccer_ball")
+    //     { 
+    //         //targetPosition = Vector3.forward;
+    //         //targetPosition = (mainCamera.transform.position - transform.position).normalized;
+    //         ball.GetComponent<Rigidbody>().AddForce((targetPosition - transform.position).normalized * force, ForceMode.Impulse);
+    //     }
+    // }
 }
