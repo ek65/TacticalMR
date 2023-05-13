32022d5fade0654c3c72c19aae53e580f79ac542
Yash <yasharsh.28@gmail.com>
2023-05-13 23:52:28 +0530

    Grounded ball motion

--- passthrough test5/Assets/Scripts/BallPhysicsManager.cs
+++ passthrough test5/Assets/Scripts/BallPhysicsManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Search;
 using UnityEngine;
+using UnityEngine.UI;
 
 /*
  * https://docs.unity3d.com/ScriptReference/Rigidbody.html
@@ -10,49 +11,44 @@ using UnityEngine;
 public class BallPhysicsManager : MonoBehaviour
 {
     Rigidbody rigidbody;
-    const float accelerationGravity = 9.8f;
+    float dynamicFriction;
+    public PhysicMaterial ballMaterial;
 
-    Vector3 initialVelocity;
-    Vector3 currentVelocity;
-    Vector3 acceleration;
-    Vector3 initialForce;
-
-    float mass;
-
-    Vector3 initialPosition;
-    Vector3 currentPosition;
+    public Vector3 force = new Vector3(0, 0, 0);
+    public Vector3 targetPosition = new Vector3(0, 0, 0);
 
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
-        mass = rigidbody.mass;
+        dynamicFriction = gameObject.GetComponent<SphereCollider>().material.dynamicFriction;
 
-        acceleration = new Vector3(0, -accelerationGravity, 0);
-        initialVelocity = Vector3.zero;
-        currentVelocity = Vector3.zero;
-
-        initialForce = mass * acceleration;
-
-        initialPosition = gameObject.transform.position;
-    }
-    float totalTime = 0;
-    void FixedUpdate()
-    {
-        // gravity
-        rigidbody.AddForce(initialForce, ForceMode.Force);
-
-        // keeping track of ball velocity at all times
-        currentVelocity = rigidbody.velocity;
     }
 
     // TODO: Motion without angular drag
-    
+
     // API for motion in xz plane
-        // Pass - slow and fast
-        // dribbling - kick with less force, happenning continuously
+    // Pass - slow and fast
+    // dribbling - kick with less force, happenning continuously
     // API for projectile motion
 
     // TODO: Motion with angular drag - implementing magnus effect
 
     // TODO: rotate ball upon motion
+
+    public void ImpartGroundedMotion()
+    {
+        Vector3 direction = targetPosition - rigidbody.position;
+        Vector3 normalizedDirection = direction.normalized;
+
+        float distance = direction.magnitude;
+        float gravity = 9.81f;
+        float initialVelocity_sq = 2*dynamicFriction*gravity*distance;
+        float initalVelocity = Mathf.Sqrt(initialVelocity_sq);
+
+        float force_magnitude = (initalVelocity * rigidbody.mass) / Time.fixedDeltaTime;
+
+        force = force_magnitude * normalizedDirection;
+
+        rigidbody.AddForce(force);
+    }
 }
