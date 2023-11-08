d4190dbda8f29facff1309be10bde19537d017b3
G3P <73323107+GP2P@users.noreply.github.com>
2023-11-07 23:18:29 -0800

    Add GameManager for Multiplayer

---
+++ UnityProject/Assets/GameManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
