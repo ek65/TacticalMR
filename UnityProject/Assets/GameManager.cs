419f214cd02a4f0a0a478e64f65bcfdf65c2880d
G3P <73323107+GP2P@users.noreply.github.com>
2023-11-07 23:19:18 -0800

    GM: Check if is host

--- UnityProject/Assets/GameManager.cs
+++ UnityProject/Assets/GameManager.cs
@@ -5,8 +5,20 @@ using Fusion;
 
 public class GameManager : MonoBehaviour
 {
+    // Start is called before the first frame update
+    private bool isHost;
+
     void Start()
     {
+        // print the current runtime type
+        Debug.Log("Runtime type: " + Application.platform);
+        
+        // Detect if we're the host
+        isHost = false;
+#if UNITY_EDITOR
+        isHost = true;
+#endif
+        Debug.Log("We are the " + (isHost ? "host" : "client"));
     }
 
     // Update is called once per frame
