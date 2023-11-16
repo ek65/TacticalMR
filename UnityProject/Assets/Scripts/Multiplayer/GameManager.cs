46a9ce16614f40b658bb57b9407d826ad588bcaa
G3P <73323107+GP2P@users.noreply.github.com>
2023-11-16 10:25:32 -0800

    GM implement rest of INetworkRunnerCallbacks interface

--- UnityProject/Assets/Scripts/Multiplayer/GameManager.cs
+++ UnityProject/Assets/Scripts/Multiplayer/GameManager.cs
@@ -116,6 +116,33 @@ public class GameManager : MonoBehaviour, INetworkRunnerCallbacks
 
 		input.Set(data);
 	}
+
+	public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+
+	public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
+
+	public void OnConnectedToServer(NetworkRunner runner) { }
+
+	public void OnDisconnectedFromServer(NetworkRunner runner) { }
+
+	public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
+
+	public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
+
+	public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+
+	public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+
+	public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+
+	public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+
+	public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data) { }
+
+	public void OnSceneLoadDone(NetworkRunner runner) { }
+
+	public void OnSceneLoadStart(NetworkRunner runner) { }
+
 // ----- Shared Spatial Anchor -----
 
 
