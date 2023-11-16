7d1f0464c6b7ea794767c611bff31d0f0e46c20a
G3P <73323107+GP2P@users.noreply.github.com>
2023-11-16 10:23:11 -0800

    Player handle network input

--- UnityProject/Assets/Scripts/Multiplayer/Player.cs
+++ UnityProject/Assets/Scripts/Multiplayer/Player.cs
@@ -2,5 +2,19 @@ using Fusion;
 
 public class Player : NetworkBehaviour
 {
-    public override void FixedUpdateNetwork(){}
+	private NetworkCharacterControllerPrototype _cc;
+
+	private void Awake()
+	{
+		_cc = GetComponent<NetworkCharacterControllerPrototype>();
+	}
+
+	public override void FixedUpdateNetwork()
+	{
+		if (GetInput(out NetworkInputData data))
+		{
+			data.Direction.Normalize(); // prevents cheating, not that we care
+			_cc.Move(5 * data.Direction * Runner.DeltaTime);
+		}
+	}
 }
