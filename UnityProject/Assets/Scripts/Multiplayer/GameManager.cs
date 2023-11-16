93cde0f6f1eac108b246ba7e73d4906be3adc2f1
G3P <73323107+GP2P@users.noreply.github.com>
2023-11-16 10:26:51 -0800

    GM reformat

--- UnityProject/Assets/Scripts/Multiplayer/GameManager.cs
+++ UnityProject/Assets/Scripts/Multiplayer/GameManager.cs
@@ -77,7 +77,7 @@ public class GameManager : MonoBehaviour, INetworkRunnerCallbacks
 		if (runner.IsServer)
 		{
 			// Create a unique position for the player
-			Vector3 spawnPosition = new Vector3((player.RawEncoded%runner.Config.Simulation.DefaultPlayers)*3,1,0);
+			Vector3 spawnPosition = new Vector3((player.RawEncoded % runner.Config.Simulation.DefaultPlayers) * 3, 1, 0);
 			NetworkObject networkPlayerObject = runner.Spawn(_playerPrefab, spawnPosition, Quaternion.identity, player);
 			// Keep track of the player avatars so we can remove it when they disconnect
 			_spawnedCharacters.Add(player, networkPlayerObject);
