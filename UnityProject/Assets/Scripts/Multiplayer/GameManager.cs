1ca1a0b050a2797c61896622f8a933faf24ea70c
G3P <73323107+GP2P@users.noreply.github.com>
2023-11-16 09:57:42 -0800

    GM Host handle player join & leave

--- UnityProject/Assets/Scripts/Multiplayer/GameManager.cs
+++ UnityProject/Assets/Scripts/Multiplayer/GameManager.cs
@@ -68,6 +68,30 @@ public class GameManager : MonoBehaviour, INetworkRunnerCallbacks
 			SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
 		});
 	}
+
+	[SerializeField] private NetworkPrefabRef _playerPrefab;
+	private Dictionary<PlayerRef, NetworkObject> _spawnedCharacters = new Dictionary<PlayerRef, NetworkObject>();
+
+	public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+	{
+		if (runner.IsServer)
+		{
+			// Create a unique position for the player
+			Vector3 spawnPosition = new Vector3((player.RawEncoded%runner.Config.Simulation.DefaultPlayers)*3,1,0);
+			NetworkObject networkPlayerObject = runner.Spawn(_playerPrefab, spawnPosition, Quaternion.identity, player);
+			// Keep track of the player avatars so we can remove it when they disconnect
+			_spawnedCharacters.Add(player, networkPlayerObject);
+		}
+	}
+
+	public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+	{
+		// Find and remove the players avatar
+		if (_spawnedCharacters.TryGetValue(player, out NetworkObject networkObject))
+		{
+			runner.Despawn(networkObject);
+			_spawnedCharacters.Remove(player);
+		}
 	}
 // ----- Shared Spatial Anchor -----
 
