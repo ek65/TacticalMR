50aab1ae3e492b0aa712e50add71be436d01c2e3
G3P <73323107+GP2P@users.noreply.github.com>
2023-12-02 17:41:19 -0800

    Fix networkedPlayer no camera issue

--- UnityProject/Assets/Scripts/Multiplayer/CharacterMovementController.cs
+++ UnityProject/Assets/Scripts/Multiplayer/CharacterMovementController.cs
@@ -10,7 +10,7 @@ public class CharacterMovementController : MonoBehaviour
     public float runningSpeed = 11.5f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
-    public Camera playerCamera;
+    // public Camera playerCamera;
     public float lookSpeed = 2.0f;
     public float lookXLimit = 45.0f;
 
@@ -67,7 +67,7 @@ public class CharacterMovementController : MonoBehaviour
         {
             rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
             rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
-            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            // playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
     }
