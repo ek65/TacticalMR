41e1a84b5b79921703eddebc2b33e1477dc218cc
Mohammed Amin <coldprohor12@gmail.com>
2024-07-12 08:40:41 -0700

    added position handling

--- UnityProject/Assets/KeyboardInput.cs
+++ UnityProject/Assets/KeyboardInput.cs
@@ -3,19 +3,29 @@ using System.Collections.Generic;
 using OpenAI.Samples.Chat;
 using TMPro;
 using UnityEngine;
-using Utilities.Audio;
-using Utilities.Encoding.Wav;
+using Whisper.Samples;
+
 public class KeyboardInput : MonoBehaviour
 {
     [SerializeField] public float moveSpeed = 4f;
     public ExitScenario exitScenario;
     private Rigidbody rb;
     private ChatBehaviour chatBehaviour;
+    private StreamingSampleMic streamingSampleMic;
 
     private TimelineManager timelineManager;
     private JSONToLLM jsonToLLM;
     public TextMeshProUGUI countdownText;
     private SynthConnect synthConnect;
+
+    private Dictionary<int, object> annotations = new Dictionary<int, object>();
+    private int clickOrder = 0;
+    private bool isAnnotationMode = false;
+    private bool isReferenceMode = false;
+    private bool isPositionMode = false;
+
+    private GameObject firstObject = null;
+    private GameObject secondObject = null;
 
     void Start()
     {
@@ -24,49 +34,148 @@ public class KeyboardInput : MonoBehaviour
         jsonToLLM = GameObject.FindGameObjectWithTag("ScenicManager").GetComponent<JSONToLLM>();
         countdownText = GameObject.FindGameObjectWithTag("countdown").GetComponent<TextMeshProUGUI>();
         chatBehaviour = GameObject.FindGameObjectWithTag("Character").GetComponent<ChatBehaviour>();
-        synthConnect = GameObject.FindGameObjectWithTag("connect").GetComponent<SynthConnect>();
+        streamingSampleMic = GameObject.FindGameObjectWithTag("stream").GetComponent<StreamingSampleMic>();
         Debug.Log("KeyboardInput script initialized");
     }
 
     void Update()
-{
-    if (Input.GetKeyDown(KeyCode.E))
     {
-        exitScenario.EndScenario();
-    }
-
-    if (Input.GetKeyDown(KeyCode.P))
-    {
-        if (timelineManager.Paused)
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            timelineManager.Unpause();
+            exitScenario.EndScenario();
         }
-        else
+
+        if (Input.GetKeyDown(KeyCode.P))
         {
-            timelineManager.Pause();
-            StartCoroutine(Countdown());
-            chatBehaviour.ToggleRecording(); // Stop recording
-            Debug.Log("Chat behaviour is:" + chatBehaviour.isRecording);
-            if (!chatBehaviour.isRecording)
+            if (timelineManager.Paused)
             {
-                StartCoroutine(ToggleRecordingCoroutine());
+                timelineManager.Unpause();
+            }
+            else
+            {
+                timelineManager.Pause();
+                StartCoroutine(Countdown());
+                chatBehaviour.ToggleRecording(); // Stop recording
+                Debug.Log("Chat behaviour is:" + chatBehaviour.isRecording);
+                if (!chatBehaviour.isRecording)
+                {
+                    StartCoroutine(ToggleRecordingCoroutine());
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            isAnnotationMode = !isAnnotationMode;
+            Debug.Log("Annotation mode: " + (isAnnotationMode ? "ON" : "OFF"));
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            isReferenceMode = !isReferenceMode;
+            firstObject = null;
+            secondObject = null;
+            Debug.Log("Reference mode: " + (isReferenceMode ? "ON" : "OFF"));
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            isPositionMode = !isPositionMode;
+            Debug.Log("Position mode: " + (isPositionMode ? "ON" : "OFF"));
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            streamingSampleMic.OnButtonPressed();
+        }
+
+        if (isAnnotationMode && Input.GetMouseButtonDown(0))
+        {
+            HandleAnnotationMode();
+        }
+
+        if (isReferenceMode && Input.GetMouseButtonDown(0))
+        {
+            HandleReferenceMode();
+        }
+
+        if (isPositionMode && Input.GetMouseButtonDown(0))
+        {
+            HandlePositionMode();
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            foreach (var annotation in annotations)
+            {
+                if (annotation.Value is GameObject)
+                {
+                    Debug.Log($"Key: {annotation.Key}, Object: {(annotation.Value as GameObject).name}");
+                }
+                else if (annotation.Value is Vector3)
+                {
+                    Debug.Log($"Key: {annotation.Key}, Reference Vector: {(Vector3)annotation.Value}");
+                }
             }
         }
     }
-    if (Input.GetKeyDown(KeyCode.I))
+
+    private void HandleAnnotationMode()
     {
-        if (chatBehaviour.isRecording)
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            chatBehaviour.ToggleRecording();
-            StartCoroutine(JSONCoroutine());
-        }
-        else
-        {
-            chatBehaviour.ToggleRecording();
+            GameObject clickedObject = hit.collider.gameObject;
+            annotations.Add(clickOrder, clickedObject);
+            streamingSampleMic.InsertAnnotationKey(clickOrder); // Insert annotation key into transcription
+            Debug.Log($"Added {clickedObject.name} to annotations with key {clickOrder}");
+            clickOrder++;
         }
     }
-    
-    // Coroutines
+
+    private void HandleReferenceMode()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            GameObject clickedObject = hit.collider.gameObject;
+
+            if (firstObject == null)
+            {
+                firstObject = clickedObject;
+                Debug.Log($"First object selected: {firstObject.name}");
+            }
+            else if (secondObject == null)
+            {
+                secondObject = clickedObject;
+                Debug.Log($"Second object selected: {secondObject.name}");
+
+                Vector3 referenceVector = secondObject.transform.position - firstObject.transform.position;
+                annotations.Add(clickOrder, referenceVector);
+                streamingSampleMic.InsertAnnotationKey(clickOrder); // Insert reference key into transcription
+                Debug.Log($"Added reference vector {referenceVector} to annotations with key {clickOrder}");
+                clickOrder++;
+
+                // Reset objects for next reference
+                firstObject = null;
+                secondObject = null;
+            }
+        }
+    }
+
+    private void HandlePositionMode()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Vector3 clickedPosition = hit.point;
+            annotations.Add(clickOrder, clickedPosition);
+            streamingSampleMic.InsertAnnotationKey(clickOrder); // Insert position key into transcription
+            Debug.Log($"Added position {clickedPosition} to annotations with key {clickOrder}");
+            clickOrder++;
+        }
+    }
+
     IEnumerator ToggleRecordingCoroutine()
     {
         Debug.Log("Started Coroutine at timestamp : " + Time.time);
@@ -74,14 +183,7 @@ public class KeyboardInput : MonoBehaviour
         chatBehaviour.ToggleRecording(); // Start recording
         Debug.Log("Finished Coroutine at timestamp : " + Time.time);
     }
-    IEnumerator SynthCoroutine()
-    {
-        Debug.Log("Started Coroutine at timestamp : " + Time.time);
-        yield return new WaitForSeconds(1);
-        synthConnect.SendExplanation(chatBehaviour.userInput);
-        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
-    }
-    
+
     IEnumerator JSONCoroutine()
     {
         Debug.Log("Started Coroutine at timestamp : " + Time.time);
@@ -91,6 +193,7 @@ public class KeyboardInput : MonoBehaviour
         StartCoroutine(ConditionCoroutine());
         Debug.Log("Finished Coroutine at timestamp : " + Time.time);
     }
+
     IEnumerator ConditionCoroutine()
     {
         Debug.Log("Started Coroutine at timestamp : " + Time.time);
@@ -100,6 +203,7 @@ public class KeyboardInput : MonoBehaviour
         StartCoroutine(ActionCoroutine());
         Debug.Log("Finished Coroutine at timestamp : " + Time.time);
     }
+
     IEnumerator ActionCoroutine()
     {
         Debug.Log("Started Coroutine at timestamp : " + Time.time);
@@ -109,6 +213,7 @@ public class KeyboardInput : MonoBehaviour
         StartCoroutine(FileCoroutine());
         Debug.Log("Finished Coroutine at timestamp : " + Time.time);
     }
+
     IEnumerator FileCoroutine()
     {
         Debug.Log("Started Coroutine at timestamp : " + Time.time);
@@ -116,36 +221,6 @@ public class KeyboardInput : MonoBehaviour
         jsonToLLM.WriteFile();
     }
 
-    if (timelineManager.Paused)
-    {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            if (!timelineManager.advancing)
-            {
-                timelineManager.rewinding = !timelineManager.rewinding;
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            if (!timelineManager.rewinding)
-            {
-                timelineManager.advancing = !timelineManager.advancing;
-            }
-        }
-        
-        // not being used rn
-        if (Input.GetKeyDown(KeyCode.Space) && !timelineManager.rewinding && !timelineManager.advancing)
-        {
-            jsonToLLM.PopulateSceneObjects();
-            jsonToLLM.CreateJSONString();
-            chatBehaviour.SubmitCombinedInput("condition"); 
-            jsonToLLM.WriteFile();
-            Debug.Log("Sent input");
-        }
-    }
-}
-    
     private IEnumerator Countdown()
     {
         countdownText.gameObject.SetActive(true);
@@ -154,12 +229,11 @@ public class KeyboardInput : MonoBehaviour
             countdownText.text = i.ToString();
             yield return new WaitForSeconds(1);
         }
-        
+
         countdownText.text = "GO";
         yield return new WaitForSeconds(1);
         countdownText.gameObject.SetActive(false);
     }
-
 
     void FixedUpdate()
     {
