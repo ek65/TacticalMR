bb1daaee3a5dea1ea070db59111f4a88d8941741
Jorge Diaz Chao <jdiazchao1911@icloud.com>
2024-07-18 16:20:39 -0700

    idk how I fixed it

--- UnityProject/Assets/Scripts/ScenicSynth/SynthNetwork.cs
+++ UnityProject/Assets/Scripts/ScenicSynth/SynthNetwork.cs
@@ -23,6 +23,17 @@ namespace SynthNetworkKit
             Debug.Log("Starting SynthNetwork.");
             db = FirebaseFirestore.DefaultInstance;
 
+            DocumentReference docRef2 = db.Collection("cities").Document("LA");
+            Dictionary<string, object> city = new Dictionary<string, object>
+            {
+                    { "Name", "Los Angeles" },
+                    { "State", "CA" },
+                    { "Country", "USA" }
+            };
+            docRef2.SetAsync(city).ContinueWithOnMainThread(task => {
+                    Debug.Log("Added data to the LA document in the cities collection.");
+            });
+
             var docRef = db.Collection("services").Document(id);
             print(docRef);
             Debug.Log("Listening to document: " + id);
