b76d67fd8b82086bb4ec928cbb8fc10749af1df7
Eddie K <ek65@berkeley.edu>
2024-08-30 16:46:22 -0700

    Auto stash before merge of "controller" and "origin/controller"

--- UnityProject/Assets/Scripts/Scene Managers/JSONToLLM.cs
+++ UnityProject/Assets/Scripts/Scene Managers/JSONToLLM.cs
@@ -476,6 +476,8 @@ public class JSONToLLM : MonoBehaviour
         File.WriteAllText(filename, jsonString);
         isAdjusted = false;
         Debug.Log($"Segment written to {filename}");
+        
+        recordingNum++;
     }
 
     
