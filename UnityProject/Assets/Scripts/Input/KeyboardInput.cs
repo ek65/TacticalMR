26b5a23c53cad6cc68b4404b6945f70b596a4417
Daniel He <tcdanielh@berkeley.edu>
2025-07-15 10:45:02 -0700

    Update KeyboardInput.cs

--- UnityProject/Assets/Scripts/Input/KeyboardInput.cs
+++ UnityProject/Assets/Scripts/Input/KeyboardInput.cs
@@ -966,11 +966,11 @@ public class KeyboardInput : NetworkBehaviour
         if (rb == null) return;
         if (timelineManager.Paused) return;
         
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        // float horizontalInput = Input.GetAxis("Horizontal");
+        // float verticalInput = Input.GetAxis("Vertical");
         
-        // float horizontalInput = 0f;
-        // float verticalInput = 0f;
+        float horizontalInput = 0f;
+        float verticalInput = 0f;
         
         Vector3 forwardDirection = transform.forward;
         movement = (forwardDirection * verticalInput + transform.right * horizontalInput).normalized * moveSpeed;
